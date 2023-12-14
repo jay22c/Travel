9c25e7aecea07bd02ce04e3b86f9ead637b27465
ijunjae <prejay@handong.ac.kr>
2023-12-15 00:33:56 +0900

    complete

--- src/main/java/com/example/Board/BoardController.java
+++ src/main/java/com/example/Board/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.Board.BoardService;
 import com.example.Board.BoardVO;
 
+
 @Controller
 @RequestMapping(value = "/board")
 public class BoardController {
